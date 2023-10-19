60c9e919d17ab4574905851e08e89de839dfdbf9
Gabriel <gabriel.einsiedel@gmail.com>
2023-10-19 10:01:07 -0300

    Added templates

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template('index.html')
+def index():
+    return render_template('index.html', name=request.args.get('name', 'Tom'))
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 if __name__ == '__main__':
-    app.home()
+    app.run(debug=True)
